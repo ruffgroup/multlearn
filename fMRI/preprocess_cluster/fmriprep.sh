05546e5b80379444c245ce7f881c9a8d92f0feeb
ella.casimiro <ella.casimiro@econ.uzh.ch>
2023-01-26 17:09:37 +0100

    dummy scans 5

--- fMRI/preprocess_cluster/fmriprep.sh
+++ fMRI/preprocess_cluster/fmriprep.sh
@@ -9,4 +9,4 @@ module load singularityce/3.10.2
 export SINGULARITYENV_FS_LICENSE=$HOME/freesurfer/license.txt
 export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
 export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
-singularity run -u -B /home/$USER/data/ds-mlearn:/data -B /scratch/$USER/workflow_folders:/workflow -B /scratch/$USER/templateflow:/opt/templateflow --cleanenv /shares/hare.econ.uzh/containers/fmriprep_20.2.3  /data /data/derivatives participant --participant-label $PARTICIPANT_LABEL  --output-spaces MNI152NLin2009cAsym T1w fsaverage fsnative  --dummy-scans 3 --skip_bids_validation -w /workflow --no-submm-recon --fmap-bspline
+singularity run -u -B /home/$USER/data/ds-mlearn:/data -B /scratch/$USER/workflow_folders:/workflow -B /scratch/$USER/templateflow:/opt/templateflow --cleanenv /shares/hare.econ.uzh/containers/fmriprep_20.2.3  /data /data/derivatives participant --participant-label $PARTICIPANT_LABEL  --output-spaces MNI152NLin2009cAsym T1w fsaverage fsnative  --dummy-scans 5 --skip_bids_validation -w /workflow --no-submm-recon --fmap-bspline
