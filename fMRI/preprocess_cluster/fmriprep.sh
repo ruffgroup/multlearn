d528c4eac41fb007e9df8e8ae16114c544fc97ea
ella.casimiro <ella.casimiro@econ.uzh.ch>
2023-01-26 14:44:04 +0100

    add fmriprep script

---
+++ fMRI/preprocess_cluster/fmriprep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=fmriprep
+#SBATCH --output=/home/ecasim/logs/res_fmriprep_%A-%a.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64G
+#SBATCH --time=24:00:00
+module load singularityce/3.10.2
+export SINGULARITYENV_FS_LICENSE=$HOME/freesurfer/license.txt
+export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
+export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
+singularity run -u -B /home/$USER/data/ds-mlearn:/data -B /scratch/$USER/workflow_folders:/workflow -B /scratch/$USER/templateflow:/opt/templateflow --cleanenv /shares/hare.econ.uzh/containers/fmriprep_20.2.3  /data /data/derivatives participant --participant-label $PARTICIPANT_LABEL  --output-spaces MNI152NLin2009cAsym T1w fsaverage fsnative  --dummy-scans 3 --skip_bids_validation -w /workflow --no-submm-recon --fmap-bspline
