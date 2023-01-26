01f6e5dc5d9494d5173f36eb4d8133d1c50e3b67
sbedi <saurabh.bedi@econ.uzh.ch>
2023-01-26 16:20:13 +0100

    variable user dir

--- fMRI/preprocess_cluster/fmriprep.sh
+++ fMRI/preprocess_cluster/fmriprep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=fmriprep
-#SBATCH --output=/home/ecasim/logs/res_fmriprep_%A-%a.txt
+#SBATCH --output=/home/$USER/logs/res_fmriprep_%A-%a.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
