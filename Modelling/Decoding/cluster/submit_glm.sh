6dc70ff399c599d93bb5e8624c1e7ffa16811489
ecasimiro <ella.casimiro@live.be>
2024-02-19 11:58:08 +0100

    conda env txt file and updated glm sh

--- Modelling/Decoding/cluster/submit_glm.sh
+++ Modelling/Decoding/cluster/submit_glm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=fit_single_glm
-#SBATCH --output=/home/cluster/gdehol/logs/fit_single_glm_%A-%a.txt
+#SBATCH --output=/home/ecasim/logs/fit_single_glm_%A-%a.txt
 #SBATCH --partition=generic
 #SBATCH --ntasks=1
 #SBATCH -c 16
@@ -8,4 +8,4 @@
 
 export PARTICIPANT_LABEL=$(printf "%02d" $SLURM_ARRAY_TASK_ID)
 
-python $HOME/git/multlearn-sns/Modelling/Decoding/fit_glm.py $PARTICIPANT_LABEL --bids_folder /shares/zne.uzh/multlearn/ds-mlearn --data_folder /shares/zne.uzh/multlearn
+python $HOME/git/multlearn-sns/Modelling/Decoding/fit_glm.py $PARTICIPANT_LABEL --bids_folder /shares/zne.uzh/multlearn/ds-mlearn --data_folder /shares/zne.uzh/multlearn --rpe_folder /shares/zne.uzh/multlearn/multlearn/bestFittingVals
